606e69902de873988a7e2b6868620208767e0446
yamid-dev <yhrodriguez1@hotmail.com>
2024-05-20 14:58:23 -0500

    Funciones en php

--- funciones.php
+++ funciones.php
@@ -10,7 +10,7 @@ echo $numero . "<br>";
 saludo();
 saludo();*/
 
-function suma(){
+/*function suma(){
     $numero_1 = 5;
     $numero_2 = 5;
 
@@ -18,6 +18,42 @@ function suma(){
 }
 suma();
 suma();
-suma();
+suma();*/
+
+/*
+FUNCIONES CON PARAMETROS
+
+function saludo($nombre){
+    echo "¡Hola ".$nombre." eres un gran programador!"."<br>";
+}
+
+saludo("Yamid");
+saludo("Katherine");
+saludo("Karen");*/
+
+function operar($num1,$num2,$operacion){
+    switch ($operacion){
+    case "sumar":
+        $resultado = $num1 + $num2;  
+        break;  
+    case "restar":
+        $resultado = $num1 - $num2;
+        break;    
+    case "multiplicar":
+        $resultado = $num1 * $num2;    
+        break;
+    case "dividir":
+        $resultado = $num1 / $num2;
+        break;    
+    default:
+        $resultado = "No se reconoce la operación";
+    }
+    return $resultado;
+}
+echo operar(15,20,"sumar") . "<br>";
+echo operar(15,20,"restar") . "<br>";
+echo operar(15,20,"multiplicar") . "<br>";
+echo operar(15,20,"dividir") . "<br>";
+
 
 ?>
