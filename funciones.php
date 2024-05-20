2726dd418110f12b587147509fc596a0070b0bdb
yamid-dev <yhrodriguez1@hotmail.com>
2024-05-20 10:59:58 -0500

    Funciones sin parametros en php

---
+++ funciones.php
@@ -0,0 +1,23 @@
+<?php
+/*function saludo(){
+    echo "Hola programador" . "<br>";
+}
+
+saludo();
+saludo();
+$numero = 5;
+echo $numero . "<br>";
+saludo();
+saludo();*/
+
+function suma(){
+    $numero_1 = 5;
+    $numero_2 = 5;
+
+    echo $numero_1 + $numero_2 . "<br>";
+}
+suma();
+suma();
+suma();
+
+?>
