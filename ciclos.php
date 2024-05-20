81cb56bdaf4fa4841b820684be58f0dd677e1427
yamid-dev <yhrodriguez1@hotmail.com>
2024-05-20 10:46:15 -0500

    Ciclo while, do_while y for

--- ciclos.php
+++ ciclos.php
@@ -3,13 +3,22 @@
 
 //Imprimir los números de 1 al 10
 //Imprimir las tablas de multiplicar;
-
+$j = 1;
 $i = 1;
 $num = 2;
-
+echo "CICLO WHILE". '<br>';
 while ($i <= 10){
     echo $i++ * $num." ";
+};
+echo '<br>'."CICLO DO-WHILE". '<br>';
+do{
+    echo $j. '<br>';
+    $j++;
+}while($j<=10);
+echo "Fin de la instrucción";
+
+echo '<br>'."CICLO FOR". '<br>';
+for($k = 1; $k <=10; $k++){
+    echo $k . '<br>';
 }
-
-
 ?>
